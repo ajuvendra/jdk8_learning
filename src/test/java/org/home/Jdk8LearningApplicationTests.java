f72b9b759f00c783049264f3c03d4d22572065cc
kumaraj <ajuvendra.kumar@tomtom.com>
2020-03-08 14:54:24 +0530

    first Commit

---
+++ src/test/java/org/home/Jdk8LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package org.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jdk8LearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
